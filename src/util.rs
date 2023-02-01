3005956e0ae0d84216c6f257e1f2d5264b2258f4
errantmind <errantmind@protonmail.com>
2023-02-01 16:04:00 -0600

    add license notice to file

--- src/util.rs
+++ src/util.rs
@@ -16,7 +16,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-
 /// Converts str of an IP address representing an internet host to a 32-bit int which represents
 /// also represents the same IP address which is in network byte order
 ///
