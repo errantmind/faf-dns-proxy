805fa26d3659975bd509f4fabe0798a717730fb9
errantmind <errantmind@protonmail.com>
2023-11-27 22:54:00 -0600

    bump dependencies and remove obsolete code
    in memory session cache is now the default, see Resumption in rustls

--- src/tls.rs
+++ src/tls.rs
@@ -16,42 +16,21 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use hashbrown::HashMap;
-
 #[inline]
 pub fn get_tls_client_config() -> tokio_rustls::rustls::ClientConfig {
-   let mut root_store = tokio_rustls::rustls::RootCertStore { roots: Vec::new() };
-   root_store.roots.extend(
+   let mut root_cert_store = tokio_rustls::rustls::RootCertStore::empty();
+   root_cert_store.add_trust_anchors(
       webpki_roots::TLS_SERVER_ROOTS
-         .0
          .iter()
          .map(|ta| tokio_rustls::rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(ta.subject, ta.spki, ta.name_constraints)),
    );
-   let mut config =
-      tokio_rustls::rustls::ClientConfig::builder().with_safe_defaults().with_root_certificates(root_store.clone()).with_no_client_auth();
-   config.session_storage = std::sync::Arc::new(PersistCache::new());
+
+   let mut config = tokio_rustls::rustls::ClientConfig::builder()
+      .with_safe_defaults()
+      .with_root_certificates(root_cert_store.clone())
+      .with_no_client_auth();
+
    config.enable_early_data = true;
 
    config
-}
-
-struct PersistCache {
-   cache: std::sync::Mutex<HashMap<Vec<u8>, Vec<u8>>>,
-}
-
-impl PersistCache {
-   fn new() -> Self {
-      PersistCache { cache: std::sync::Mutex::new(HashMap::new()) }
-   }
-}
-
-impl tokio_rustls::rustls::client::StoresClientSessions for PersistCache {
-   fn put(&self, key: Vec<u8>, value: Vec<u8>) -> bool {
-      self.cache.lock().unwrap().insert(key, value);
-      true
-   }
-
-   fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
-      self.cache.lock().unwrap().get(key).cloned()
-   }
 }
