17fba407268fbbb38682460b05cb96d37358ad0a
errantmind <errantmind@protonmail.com>
2024-01-13 20:33:40 -0600

    delete linear chart

--- src/chart.rs
+++ src/chart.rs
@@ -1,46 +1,5 @@
 /// Generate a chart of the latencies.
 #[inline]
-pub fn _generate_linear_chart(latencies: Vec<u64>) -> anyhow::Result<(), Box<dyn std::error::Error>> {
-   assert!(!latencies.is_empty());
-
-   // The latencies must already be sorted.
-   let max_value = *latencies.last().unwrap();
-   let mut range_size: u64 = 5;
-   let get_num_ranges = |max_value: u64, range_size: u64| (max_value / range_size) as usize + 1;
-
-   while get_num_ranges(max_value, range_size) > 10 {
-      range_size *= 2;
-   }
-   let num_ranges = get_num_ranges(max_value, range_size);
-
-   let mut counts = vec![0; num_ranges];
-   for &value in &latencies {
-      // Determine which range the value falls into based on its value
-      let range_index = (value / range_size) as usize;
-      counts[range_index] += 1;
-   }
-
-   let ranges = (0..num_ranges).map(|i| format!("[{},{})ms", i as u64 * range_size, (i as u64 + 1) * range_size)).collect::<Vec<_>>();
-
-   let chart = charming::Chart::new()
-      .title(
-         charming::component::Title::new().text("Latency Distribution").left("center").subtext("timed: question -> answer").left("center"),
-      )
-      .x_axis(charming::component::Axis::new().type_(charming::element::AxisType::Category).data(ranges))
-      .y_axis(charming::component::Axis::new().type_(charming::element::AxisType::Value).name("count"))
-      .series(charming::series::Bar::new().data(counts));
-
-   let mut renderer = charming::HtmlRenderer::new("latency distribution", 1800, 720);
-
-   // Save the chart as HTML file.
-   let file_path = get_chart_file_path()?;
-   renderer.save(&chart, &file_path).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-   println!("chart saved to {:?}", &file_path);
-
-   Ok(())
-}
-
-#[inline]
 pub fn generate_log_chart(latencies: Vec<u64>) -> anyhow::Result<(), Box<dyn std::error::Error>> {
    assert!(!latencies.is_empty());
 
